82eaed713b0758f6648bb4377e50c6a94cacbbbf
JOdiemo <odiemo@gmail.com>
2022-08-11 06:17:37 +0300

    Changes on style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+// display the fetched data
+const displayData = (allMeals) => {
+  let output = '';
+  allMeals.forEach((meal) => {
+    output += `<div id="meals-gallery">
+          <img src="${meal.strCategoryThumb}" />
+                <h3>${meal.strCategory}</h3><h4>Likes (<span>0</span>)
+                <i class="fa-regular fa-heart"></i><br>
+                <button id="comments">Comment</button>
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+                </div>`;
+  });
 
-  return element;
-}
+  document.getElementById('meal').innerHTML = output;
+};
 
-document.body.appendChild(component());
+// fetch data from the base API
+const getMeals = async () => {
+  await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then((res) => res.json())
+    .then((data) => {
+      displayData(data.categories);
+    });
+};
+
+document.addEventListener('DOMContentLoaded', getMeals);
