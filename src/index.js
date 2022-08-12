75352bbb191d8618e5a50db6d53541a7e14d2c1b
PNdunguMaina <ndunguwamaina@gmail.com>
2022-08-12 12:54:37 +0300

    Create popup with meal details and comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,71 @@
-import _ from 'lodash';
 import './style.css';
+import { findMeals, postComments } from './modules/getPostData';
+import {
+  displayPopUp,
+  showComments,
+  openPopup,
+  closePopup,
+  commentsContainer,
+} from './modules/userInterface';
+
+const mealsContainer = document.querySelector('#meal');
+const popupWindow = document.getElementById('pop-up');
+const form = document.querySelector('form');
+const input = document.getElementById('user-name');
+const textarea = document.getElementById('suggestion');
+const closeBtn = document.querySelector('.close-button');
+
+const displayMeals = async () => {
+  const meals = await findMeals();
+  meals.forEach((meal) => {
+    mealsContainer.innerHTML += `
+    <div id="${meal.idCategory}">
+      <div class="image-cont">
+        <img
+          src=${meal.strCategoryThumb}
+          alt="food image"
+        />
+      </div>
+      <div>
+        <div><p>${meal.strCategory}</p></div>
+      <button class="comment-btn" type="button" id="${meal.idCategory}">Comments</button>
+  </div>
+  </div>
+        `;
+    document.querySelectorAll('.comment-btn').forEach((button) => {
+      button.addEventListener('click', openPopup);
+    });
+  });
+};
+
+mealsContainer.addEventListener('click', (event) => {
+  const commentBtn = event.target;
+  if (!commentBtn.classList.contains('comment-btn')) return;
+  const id = commentBtn.getAttribute('id');
+  displayPopUp(id);
+  showComments(id);
+  openPopup(popupWindow);
+});
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const { id } = event.target.dataset;
+  if (input.value && textarea.value) {
+    postComments(id, input.value, textarea.value);
+    commentsContainer({
+      creation_date: '',
+      username: input.value,
+      comment: textarea.value,
+    });
+    input.value = '';
+    textarea.value = '';
+  }
+});
+
+closeBtn.addEventListener('click', () => {
+  closePopup(popupWindow);
+});
+displayMeals();
 
 function component() {
   const element = document.createElement('div');
---
+++ src/modules/getPostData.js
@@ -0,0 +1,37 @@
+const mealUrl = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+const baseUrl =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+const appId = 'VLkvQrH2CamIWyKQoVQw';
+const commentUrl = `${baseUrl}apps/${appId}/comments?item_id=`;
+const postCommentUrl = `${baseUrl}apps/${appId}/comments/`;
+
+// fetch food details from the base API
+const findMeals = async () => {
+  const response = await fetch(mealUrl);
+  const { categories: food } = await response.json();
+  return food;
+};
+
+// fetch comment details from the involvement API
+const fetchComments = async (id) => {
+  const response = await fetch(commentUrl + id);
+  const allComments = await response.json();
+  return allComments;
+};
+
+// post comment details to the involvement API
+const postComments = async (id, name, suggestion) => {
+  await fetch(postCommentUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: id,
+      username: name,
+      comment: suggestion,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((data) => data.json());
+};
+
+export { findMeals, fetchComments, postComments };
