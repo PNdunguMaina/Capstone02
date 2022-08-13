1846045a0b1c7bd9133531f07af1eec79d6d5a7b
PNdunguMaina <ndunguwamaina@gmail.com>
2022-08-13 13:15:24 +0300

    Fix ESLint errors

--- src/_test_/getMeals.test.js
+++ src/_test_/getMeals.test.js
@@ -1,14 +1,13 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 
 test('properly return total number of meals displayed', () => {
-    // fetch food details from the base API
-    const findMeals = () => {
-        const response = fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
-        const { categories: food } = response.json();
-        console.log(food);
-
-        expect(food.length).toBe(14);
-    };
-
+  const findMeals = () => {
+    const response = fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
+    const { categories: food } = response.json();
+    const length = food.length;
+    console.log(length);
+    expect(findMeals()).toBe(14);
+    };    
 })
